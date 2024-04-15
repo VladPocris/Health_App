987d1fad8268f322e3869a6babbc06049ebc6ff8
Ryan <X00183272@mytudublin.ie>
2024-04-15 13:12:46 +0100

    calander

--- health_app/urls.py
+++ health_app/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from calander import views  
+ 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'), 
+    path('all_events/', views.all_events, name='all_events'), 
+    path('add_event/', views.add_event, name='add_event'), 
+    path('update/', views.update, name='update'),
+    path('remove/', views.remove, name='remove'),
 ]
