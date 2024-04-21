e693d0798278c9dbacfbc6ae9a79deacd711f865
x00188120 <x00188120@mytudublin.ie>
2024-04-21 18:25:50 +0100

    calendar fixes

--- calendar/views.py
+++ calendar/views.py
@@ -10,18 +10,20 @@ def calendar(request):
     }
     return render(request,'calendar.html',context)
  
-def all_events(request):                                                                                                 
-    all_events = Events.objects.all()                                                                                    
-    out = []                                                                                                             
-    for event in all_events:                                                                                             
-        out.append({                                                                                                     
-            'title': event.name,                                                                                         
-            'id': event.id,                                                                                              
-            'start': event.start.strftime("%m/%d/%Y, %H:%M:%S"),                                                         
-            'end': event.end.strftime("%m/%d/%Y, %H:%M:%S"),                                                             
-        })                                                                                                               
-                                                                                                                      
-    return JsonResponse(out, safe=False) 
+def all_events(request):
+    all_events = Events.objects.all()
+    out = []
+
+    for event in all_events:
+        start_time = event.start.strftime("%m/%d/%Y, %H:%M:%S") if event.start is not None else None
+        end_time = event.end.strftime("%m/%d/%Y, %H:%M:%S") if event.end is not None else None
+
+        out.append({
+            'title': event.name,
+            'id': event.id,
+            'start': start_time,
+            'end': end_time,
+        })
  
 def add_event(request):
     start = request.GET.get("start", None)
