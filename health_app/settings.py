13e1c99558c69b276794b3bbb2c5eb0af14b6f08
Vlad Pocris <x00202628@mytudublin.ie>
2024-04-23 09:11:09 +0100

    Repository fixed, calendar issues styling fixed, previous code restored after bad push, bugs with calendar fixed, set the footer always on top

--- health_app/settings.py
+++ health_app/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'health_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'django',
         'USER': 'root',
         'PASSWORD': 'Health_app',
