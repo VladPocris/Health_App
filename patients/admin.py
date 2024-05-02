7161c100ea52cbd277c52b81cce7d30dc4f5b0f7
x00188120 <x00188120@mytudublin.ie>
2024-05-02 19:51:00 +0100

    Database updates

--- patients/admin.py
+++ patients/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Category, Medicine, Patient, Prescription
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Medicine)
+admin.site.register(Patient)
+admin.site.register(Prescription)
--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Patient(models.Model):
+    first_name= models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_num = models.CharField(max_length=10)
+    email = models.CharField(max_length=50)
+
+
+class Medicine(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=300, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/Medicine/')
+
+    def __str__(self):
+        return self.name
+
+class Prescription(models.Model):
+    medicine = models.ForeignKey(Medicine, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    dosage = models.IntegerField(default=1)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.medicine
